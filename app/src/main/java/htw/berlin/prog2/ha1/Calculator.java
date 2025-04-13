903af6875083b32a1cfcf815c59b951ab9adafa4
NickWeber651 <N.Weber@Student.HTW-Berlin.de>
2025-04-13 15:55:37 +0200

    Kehrwert ergibt bei Null ein Error

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -77,7 +77,13 @@ public class Calculator {
         var result = switch(operation) {
             case "√" -> Math.sqrt(Double.parseDouble(screen));
             case "%" -> Double.parseDouble(screen) / 100;
-            case "1/x" -> 1 / Double.parseDouble(screen);
+            case "1/x" -> {
+                double input = Double.parseDouble(screen);
+                if (input == 0) yield Double.NaN;
+                else yield 1 / input;
+            }
+
+
             default -> throw new IllegalArgumentException();
         };
         screen = Double.toString(result);
